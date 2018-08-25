25f712238016f5a926af47fba8e214cf6a8a3ef9
Oleg Khalin <olegkhalin18@gmail.com>
2018-08-25 10:50:24 +0300

    Updating database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: 'postgresql-infinite-39359',
-		user: 'postgres',
-		password: '',
-		database: 'face-recognition-app'
+		host: process.env.DATABASE_URL,
+		ssl: true,
 	}
 });
 
